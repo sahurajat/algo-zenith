9c0fcbd7128c4098521b970062e2a0edc4362e4e
Sarthak Kalia <sarthakkalia45@gmail.com>
2024-05-20 11:08:49 +0530

    Special Array I LeetCode Problem

--- 22.java
+++ 22.java
@@ -1,6 +1,5 @@
 An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
 You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
-
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         if(nums.length == 1) return true;
